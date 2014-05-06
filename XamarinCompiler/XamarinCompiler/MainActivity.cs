b10cae514be3a3349443718c8f3c867de5ddfc49
vindal0012 <ianthonycatabran@hotmail.com>
2014-05-06 10:39:36 +0930

    XamarinCompiler

---
+++ XamarinCompiler/XamarinCompiler/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinCompiler
+{
+	[Activity (Label = "XamarinCompiler", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.btn);
+			
+			button.Click += delegate {
+				button.Text = string.Format ("{0} clicks!", count++);
+			};
+		}
+	}
+}
+
+
